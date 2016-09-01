6b9a672135bca50825df362a5867e667771c331b
Robb Shecter <robb.shecter@gsa.gov>
2016-09-01 15:56:39 -0700

    fixed deprecation warnings

--- spec/omniauth/strategies/cg_spec.rb
+++ spec/omniauth/strategies/cg_spec.rb
@@ -7,15 +7,15 @@ describe OmniAuth::Strategies::Cg do
 
   context 'client options' do
     it 'should have correct name' do
-      subject.options.name.should eq('cg')
+      expect(subject.options.name).to eq('cg')
     end
 
     it 'should have correct site' do
-      subject.options.client_options.site.should eq('https://cloud.gov')
+      expect(subject.options.client_options.site).to eq('https://cloud.gov')
     end
 
     it 'should have correct token url' do
-      subject.options.client_options.token_url.should eq('/oauth/authorize')
+      expect(subject.options.client_options.token_url).to eq('/oauth/authorize')
     end
   end
 end
