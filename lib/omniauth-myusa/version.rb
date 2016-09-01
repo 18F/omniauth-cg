6be2007653b4c2e7e22f6c924c69b21f4e851275
Robb Shecter <robb.shecter@gsa.gov>
2016-08-31 17:35:39 -0700

    Updated the tests and version

--- lib/omniauth-myusa/version.rb
+++ lib/omniauth-myusa/version.rb
@@ -1,5 +1,5 @@
 module Omniauth
   module Myusa
-    VERSION = "0.0.4"
+    VERSION = "0.0.1"
   end
 end
--- spec/omniauth/strategies/cg_spec.rb
+++ spec/omniauth/strategies/cg_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
-describe OmniAuth::Strategies::Myusa do
+describe OmniAuth::Strategies::Cg do
   subject do
-    OmniAuth::Strategies::Myusa.new({})
+    OmniAuth::Strategies::Cg.new({})
   end
 
-  context "client options" do
+  context 'client options' do
     it 'should have correct name' do
-      subject.options.name.should eq("myusa")
+      subject.options.name.should eq('cg')
     end
 
     it 'should have correct site' do
-      subject.options.client_options.site.should eq('https://alpha.my.usa.gov')
+      subject.options.client_options.site.should eq('https://cloud.gov')
     end
 
     it 'should have correct token url' do
