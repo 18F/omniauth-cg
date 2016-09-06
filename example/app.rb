1609cb402c2216760459ceec3a3d650c41e4ca7a
Robb Shecter <robb.shecter@gsa.gov>
2016-09-06 15:46:51 -0700

    Auth is working

--- example/app.rb
+++ example/app.rb
@@ -13,7 +13,7 @@ get '/' do
     <link href='//fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <link href='//s3.amazonaws.com/myusa-static/button.min.css' rel='stylesheet' type='text/css'>
     <p>
-      <a href="https://login.cloud.gov/oauth/authorize" class="btn btn-social btn-myusa">Connect with Cloud.gov</a>
+      <a href="https://login.cloud.gov/oauth/authorize?client_id=#{ENV.fetch('APP_ID')}&response_type=code" class="btn btn-social btn-myusa">Connect with Cloud.gov</a>
     </p>
   </body>
 </html>
--- example/config.ru
+++ example/config.ru
@@ -14,8 +14,8 @@ use Rack::Session::Cookie, secret: 'secret'
 # to `provider :cg ...`
 #
 # Uncomment the following line with the url of this dummy app.
-# OmniAuth.config.full_host = "http://dummy.example.com"
-#
+OmniAuth.config.full_host = 'http://localhost:9292/'
+
 # To run the app, simply call `rackup` from the command line. If you would like to run as
 # a daemon, call `rackup -D` ... if you do this, you should set up logging.
 
